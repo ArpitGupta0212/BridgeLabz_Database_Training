c972eb1a6c7c1d0516f763afb462def8cc2bad79
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-26 16:13:58 +0530

    [Arpit Gupta] Java Maven Jdbc project Complete

---
+++ greeting_jdbc_template_app/src/main/java/com/reeting_jdbc_template_app/App.java
@@ -0,0 +1,13 @@
+package com.reeting_jdbc_template_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
